662a74ae90f4ab4569439d52ed0eb55f3468e929
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-10-11 12:23:43 +0000

    added github action

---
+++ .github/scripts/update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+NEXUS_URL="https://api.github.com/repos/sonatype/nexus-public/releases"
+
+
+FULL_LAST_VERSION=$(curl -SsL ${NEXUS_URL} | jq .[0].name -r )
+LAST_VERSION="${FULL_LAST_VERSION:9}"
+
+sed -i -e "s|NEXUS_VERSION='.*'|NEXUS_VERSION='${LAST_VERSION}'|" Dockerfile*
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "update to version: ${LAST_VERSION}"
+  git push
+fi
